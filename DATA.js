5d83f1ff97a143288a33d6155e1384132ef135b7
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-10 12:15:20 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,40 @@
+export const item = {
+    "id": "5d627f9a-3582-44c4-861b-ab4411a0c63f",
+    "top_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/219.png",
+    "redirect_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/220.png",
+    "destination_title": "Could arrive consumer either according couple child. See language today owner group rise thought. Field agency sound share.",
+    "sub_title": "red",
+    "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/101.png",
+    "summer_destinations": [
+        {
+            "id": "ad7ac0e9-218e-46d5-b2e2-7620f7624a99",
+            "destination_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/36.png",
+            "menu_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/215.png",
+            "destination": "moment"
+        },
+        {
+            "id": "ddc41cad-6b84-4b84-a823-ae41b14c941b",
+            "destination_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/181.png",
+            "menu_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/94.png",
+            "destination": "myself"
+        },
+        {
+            "id": "05ffe8c3-1cab-4bd7-a25b-0ccec9aac224",
+            "destination_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/294.png",
+            "menu_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/368.png",
+            "destination": "visit"
+        },
+        {
+            "id": "32e8a56f-4bb2-4310-b2f2-a7e4871b1eba",
+            "destination_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/210.png",
+            "menu_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/350.png",
+            "destination": "where"
+        },
+        {
+            "id": "04121859-2da5-46a0-81ec-8ceccf960158",
+            "destination_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/264.png",
+            "menu_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/153.png",
+            "destination": "figure"
+        }
+    ]
+}
